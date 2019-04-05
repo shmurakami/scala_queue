c3eb8e969dc840ca78fe548eda7d3df1f87ca576
shaka <shmurakami1104@gmail.com>
2019-04-06 00:22:13 +0900

    update

--- build.sbt
+++ build.sbt
@@ -11,5 +11,6 @@ libraryDependencies ++= Seq(
   "org.mockito" % "mockito-core" % "2.13.0" % "test",
   "com.typesafe.akka" %% "akka-http"   % "10.1.8",
   "com.typesafe.akka" %% "akka-stream" % "2.5.19",
+  "com.typesafe.akka" %% "akka-http-spray-json" % "10.1.8",
 )
 
--- src/main/scala/com/shmrkm/queue/provider/ProviderServer.scala
+++ src/main/scala/com/shmrkm/queue/provider/ProviderServer.scala
@@ -1,25 +1,64 @@
 package com.shmrkm.queue.provider
 
+import akka.Done
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import spray.json.DefaultJsonProtocol._
+import spray.json.RootJsonFormat
 
-import scala.concurrent.ExecutionContextExecutor
+import scala.concurrent.{ExecutionContextExecutor, Future}
 import scala.io.StdIn
 
 object ProviderServer {
+  implicit val system: ActorSystem = ActorSystem("queue-provider")
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+
+  var orders: List[Item] = Nil
+
+  final case class Item(name: String, id: Long)
+  final case class Order(items: List[Item])
+
+  implicit val itemFormat: RootJsonFormat[Item] = jsonFormat2(Item)
+  implicit val orderFormat: RootJsonFormat[Order] = jsonFormat1(Order)
+
+  def fetchItem(itemId: Long): Future[Option[Item]] = Future {
+    orders.find(_.id == itemId)
+  }
+
+  def saveOrder(order: Order): Future[Done] = {
+    orders = order match {
+      case Order(items) => items ::: orders
+      case _ => orders
+    }
+
+    Future {Done}
+  }
 
   def run(): Unit = {
-    implicit val system: ActorSystem = ActorSystem("queue-provider")
-    implicit val materializer: ActorMaterializer = ActorMaterializer()
-    implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
     val route =
-      path("hello") {
-        get {
-          complete(HttpEntity(ContentTypes.`application/json`, """{"say":"hello"}"""))
+      get {
+        pathPrefix("item" / LongNumber) { id =>
+          val maybeItem: Future[Option[Item]] = fetchItem(id)
+          onSuccess(maybeItem) {
+            case Some(item) => complete(item)
+            case None => complete(StatusCodes.NotFound)
+          }
+        }
+      } ~
+        post {
+        path("create-order") {
+          entity(as[Order]) { order =>
+            val saved: Future[Done] = saveOrder(order)
+            onComplete(saved) { done =>
+              complete("""{"result":true}""")
+            }
+          }
         }
       }
 
