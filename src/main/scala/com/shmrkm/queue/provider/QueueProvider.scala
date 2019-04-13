1a5e3c6232c6c3acf8b72c395da4b93569e88389
shaka <shmurakami1104@gmail.com>
2019-04-13 14:26:25 +0900

    sample resource

--- src/main/scala/com/shmrkm/queue/provider/QueueProvider.scala
+++ src/main/scala/com/shmrkm/queue/provider/QueueProvider.scala
@@ -2,10 +2,10 @@ package com.shmrkm.queue.provider
 
 object QueueProvider {
   def main(args: Array[String]): Unit = {
-//    ProviderServer.run()
-//    ProviderServer.streaming()
-//    ProviderServer.actor()
-//    ProviderServer.flow()
-    ProviderServer()
+    //    ProviderServer.run()
+    //    ProviderServer.streaming()
+    //    ProviderServer.actor()
+    //    ProviderServer.flow()
+        ProviderServer()
   }
 }
